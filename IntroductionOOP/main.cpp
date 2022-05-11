15aa2f32ef44a0054b86d5905d0bea7daad849d6
Fukatsummy <101829056+Fukatsummy@users.noreply.github.com>
2022-05-11 20:48:16 +0300

    IntroductionToOOP

--- IntroductionOOP/main.cpp
+++ IntroductionOOP/main.cpp
@@ -1,9 +1,29 @@
 #include<iostream>
 using namespace std;
 
+struct Point
+{
+	double x;// Характеристики
+	double y;
+};
+
 void main()
 {
 
 	setlocale(LC_ALL, "");
 
+	int a;// объявление переменной 'а' типа int
+	Point A;//Объект//Объявление переменной 'А' типа 'Point'
+	        //Объявление объекта 'А' структуры 'Point'
+	        //Создание экземпляра 'А' структуры 'Point'
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+
+	Point* pA = &A;
+	cout << pA->x << "\t" << pA->y << endl;
+	cout << (*pA).x << "\t" << (*pA).y << endl;//второй вариант
+
+	cout << sizeof(Point) << endl; //Размер
+	cout << typeid(A).name() << endl;
 }
