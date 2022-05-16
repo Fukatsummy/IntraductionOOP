a7f5b5128e837af6fc871c15f7996494f9619652
Fukatsummy <101829056+Fukatsummy@users.noreply.github.com>
2022-05-16 22:18:46 +0300

    Constructor

--- IntroductionOOP/main.cpp
+++ IntroductionOOP/main.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 using std::cout;
 
+
+#define delimiter "\n------------------\n"
 class Point
 {
 	double x;// Характеристики
@@ -25,19 +27,47 @@ public:
 	}
 
 	//       Constructors:    
-	Point()
+
+	//Point()
+	//{
+	//	x = y = 0;//зануляет переменные
+	//	cout << "DefaultConstructor:\t" << this << endl;
+	//}
+	/*Point(double x)
 	{
-		x = y = 0;
-		cout << "DefaultConstructor:\t" << this << endl;
+		this->x = x;
+		this->y = 0;
+		cout << "Single argument constructor:" << this << endl;
+	}*/
+	Point(double x=0, double y=0)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t" << this << endl;
+	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:\t" << this<<endl;
 	}
 	~Point()
 	{
-		cout << "Destructor:\t\t" << this << endl;
+		cout << "Destructor:\t" << this << endl;
+	}
+
+	//       Operators:
+	Point operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
 	}
 
 
 	//       Methods:
-	double distance(Point other)
+	double distance(const Point& other)
 	{
 		//this - точка , для которой вызвался метод(от которой нужно найти расстояние)
 		//other - точка, до которой нужно найти расстояние
@@ -64,6 +94,9 @@ double distance(Point A, Point B)
 //#define STRUCT_POINT
 //#define CLASS_WORK
 //#define TASK_1
+//#define CONSTRUCTOR_CHECK
+//#define ASSIGNMENT_CHECK_1
+#define ASSIGNMENT_CHECK_2
 
 void main()
 {
@@ -101,10 +134,13 @@ void main()
 	B.set_x(7);
 	B.set_y(8);
 	cout << B.get_x() << "\t" << B.get_y() << endl;
-
+	cout << delimiter;
 	cout << "Растояние от точки А до точки В: " << A.distance(B) << endl;
+	cout << delimiter;
 	cout << "Растояние от точки А до точки В: " << B.distance(A) << endl;
+	cout << delimiter;
 	cout << "Растояние между точками А и В: " << distance(A,B) << endl;
+	cout << delimiter;
 	cout << "Растояние между точками А и В: " << distance(B,A) << endl;
 #endif // CLASS_WORK
 
@@ -120,14 +156,52 @@ void main()
 	cout << Distance(A, B) << endl;
 #endif // TASK_1
 
+#ifdef CONSTRUCTOR_CHECK
+	int a(2);
+	cout << a << endl;
 
-	int a;
 	Point A;
 	A.print();
 	//cout << A.get_x() << "\t" << A.get_y() << endl;
+
+	Point B = 5;//sindle argument constructor
+	B.print();
+
+	Point C(8);//sindle argument constructor
+	C.print();
+
+	Point D(2, 3);
+	D.print();
+
+	Point E = D;//Copy Constructor
+	E.print();
+
+	Point F(D);//Copy Constructor
+	F.print();
+#endif // CONSTRUCTOR_CHECK
+
+#ifdef ASSIGNMENT_CHECK_1
+	Point A(2, 3);
+	Point B = A;  //Copy Constructor потому что создается объект 'В'
+	Point C; // default constructor
+	C = B;  //Copy Assignment потому что объект 'С' уже существует  
+#endif // ASSIGNMENT_CHECK_1
+
+#ifdef ASSIGNMENT_CHECK_2
+
+	int a, b, c;
+	a = b = c = 0;
+	cout << a << "\t" << b << "\t" << c << endl;
+
+	Point A, B, C;
+	A = B = C = Point(2, 3);
+	A.print();
+#endif // ASSIGNMENT_CHECK_2
+
+
 }
 
-double Distance(Point A, Point B)
+double Distance(const Point& A,const Point& B)
 {
 	return sqrt((A.get_x() - B.get_x())*(A.get_x() - B.get_x()) + (A.get_y() - B.get_y())*(A.get_y() - B.get_y()));
 }
